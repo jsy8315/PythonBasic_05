00aa2cc71906e767a7e9b3b1038efd551a35d61a
Bene S <110025650+jsy8315@users.noreply.github.com>
2022-12-13 02:34:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 기본문
+a = 5
+
+if a >= 0:
+    print("a >= 0")
+elif a >= -10:
+    print("0 > a >= -10")
+else:
+    print("-10 > a")
+
+# 
+a = 15
+if a <= 20 and a>=0:
+    print("Yes")
+
+# 조건문 기타 연산자
+
+# x in 리스트
+
+# x not in 문자
